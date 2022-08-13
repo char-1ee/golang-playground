e5d69e8ad60bd9077852c607dda569db317f41ef
char-1ee <xingjianli59@gmail.com>
2022-08-13 10:09:42 +0800

    chore: update images source

--- pull-images.sh
+++ pull-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker pull soulteary/golang-playground:web-1.19.0
-docker pull soulteary/golang-playground:sandbox-1.19.0
-docker pull soulteary/golang-playground:actuator-1.19.0
+docker pull char-1ee/golang-playground:web-1.19.0
+docker pull char-1ee/golang-playground:sandbox-1.19.0
+docker pull char-1ee/golang-playground:actuator-1.19.0
 docker pull memcached:1.6.15-alpine
